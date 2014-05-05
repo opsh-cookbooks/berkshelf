862bac31c98f506aebbe075c89d0c3b52b2e833a
gankkank <gankkank@gmail.com>
2014-05-05 12:55:43 +0800

    berkshelf for ubuntu 12.04 and 14.04

---
+++ README.md
@@ -0,0 +1,12 @@
+# berkshelf-install-sh
+
+### note
+
+* dep-selector-libgecode-1.0.0 require gecode ~> 3.5, but 3.7 also ok.
+
+* use system gecode library to install dep-selector-libgecode-1.0.0 is faster.
+
+### usage
+
+* `ops-dev recipe berkshelf-install-sh`
+
---
+++ recipes/default.sh
@@ -0,0 +1,41 @@
+#need: gem
+which berks &> /dev/null
+if [ $? -eq 0 ]
+then
+  echo.info "berkshelf already installed"
+  exit 1
+fi
+
+case "${node[os]} ${node[version]}" in
+ubuntu*)
+  apt-get update && apt-get install -y curl git vim
+  ;;
+*)
+  exit 1
+  ;;
+esac
+
+
+case "${node[os]} ${node[version]}" in
+ubuntu*12.04*)
+    ldconfig -v | grep gecode &> /dev/null
+    if [ $? -ne 0 ]; then
+      apt-get install -y libgecode30 libgecode-dev
+    fi
+;;
+ubuntu*14.04*)
+    ldconfig -v | grep gecode &> /dev/null
+    if [ $? -ne 0 ]; then
+      apt-get install build-essential
+      [ -f "gecode-3.7.3.tar.gz" ] || wget http://www.gecode.org/download/gecode-3.7.3.tar.gz
+      tar zxf gecode-3.7.3.tar.gz
+      cd gecode-3.7.3 && ./configure && make && make install
+      ldconfig
+    fi
+;;
+esac
+
+
+USE_SYSTEM_GECODE=1 gem install dep-selector-libgecode --no-ri --no-rdoc
+gem install berkshelf --no-ri --no-rdoc 
+gem install bundler --no-ri --no-rdoc 
